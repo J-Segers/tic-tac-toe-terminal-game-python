510ab841ca44df53752b09a27faf01fb3e38f62c
J-Segers <jethrosegers@msn.com>
2023-02-01 16:46:08 +0100

    changed p1 and p2 to dicts and added a current player for swapping between players each turn. added draw_board function.

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,17 +1,31 @@
 class TicTacToe:
     board_empty = [
-        [" 1 ", "║", " 2 ", "║", " 3 ",],
-        ["═══", "╬", "═══", "╬", "═══",],
-        [" 4 ", "║", " 5 ", "║", " 6 ",],
-        ["═══", "╬", "═══", "╬", "═══",],
-        [" 7 ","║", " 8 ", "║", " 9 ",]
+        ["         ", "║", "         ", "║", "         "],
+        ["    1    ", "║", "    2    ", "║", "    3    ",],
+        ["         ", "║", "         ", "║", "         "],
+        ["═════════", "╬", "═════════", "╬", "═════════"],
+        ["         ", "║", "         ", "║", "         "],
+        ["    4    ", "║", "    5    ", "║", "    6    ",],
+        ["         ", "║", "         ", "║", "         "],
+        ["═════════", "╬", "═════════", "╬", "═════════"],
+        ["         ", "║", "         ", "║", "         "],
+        ["    7    ", "║", "    8    ", "║", "    9    ",],
+        ["         ", "║", "         ", "║", "         "]
     ]
+
+    p1 = dict()
+    p2 = dict()
 
     def __init__(self):
         self.board = TicTacToe.board_empty
         self.draw_title()
-        self.p1 = input("enter name player one: ")
-        self.p2 = input("enter name player two: ")
+        self.p1["name"] = input("enter name player one: ")
+        self.p1["mark"] = "x"
+        self.p2["name"] = input("enter name player two: ")
+        self.p2["mark"] = "o"
+        self.current_player = self.p1
+        print(self.current_player)
+
 
     def draw_title(self):
         print("""
@@ -24,4 +38,13 @@ class TicTacToe:
                                                                                                   
 
 """)
-    
+
+    def draw_board(self):
+        current_board = ""
+
+        for row in self.board:
+            # spacing centers game board under title
+            current_board += "\t\t\t\t" + ''.join(row)
+            current_board += '\n'
+        
+        print(current_board)
